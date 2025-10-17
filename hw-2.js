98211ff1f95f3f897bd20eab7b1bbaaf2e76e938
MirDimAnsk <mda405@mail.ru>
2025-10-17 17:14:18 +0700

    JS task 6 completed

--- hw-2.js
+++ hw-2.js
@@ -1,13 +1,13 @@
 // Задание 1
 
-let a = 10;
+let a=10;
 alert(a);
-a = 20;
+a=20;
 alert(a);
 
 // Задание 2
 
-let firstIphone = 2007;
+let firstIphone=2007;
 alert(`Первый Iphone вышел в ${firstIphone} году.`);
 
 // Задание 3
@@ -17,12 +17,17 @@ alert(`Создатель языка JS ${creatorJS}.`);
 
 // Задание 4
 
-a = 10;
-let b = 2;
+a=10;
+let b=2;
 alert(`Сумма: ${a+b}, Разность: ${a-b}, Произведение: ${a*b}, Частное: ${a/b}`);
 
 // Задание 5
 
-let result = 2**5;
+let result=2**5;
 alert(result);
 
+// Задание 6
+
+a=9;
+result=a%b;
+alert(`Остаток от деления: ${result}.`);
