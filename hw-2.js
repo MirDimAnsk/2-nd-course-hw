22da16eadef8b8b2974e2f261a540ed358f161ff
MirDimAnsk <mda405@mail.ru>
2025-10-17 17:03:51 +0700

    JS task 4 completed

--- hw-2.js
+++ hw-2.js
@@ -1,16 +1,23 @@
 // Задание 1
 
 let a = 10;
-alert (a);
+alert(a);
 a = 20;
-alert (a);
+alert(a);
 
 // Задание 2
 
 let firstIphone = 2007;
-alert (`Первый Iphone вышел в ${firstIphone} году.`);
+alert(`Первый Iphone вышел в ${firstIphone} году.`);
 
 // Задание 3
 
 const creatorJS = 'Брэндан Эйх (Brendan Eich)';
-alert (`Создатель языка JS ${creatorJS}.`);
+alert(`Создатель языка JS ${creatorJS}.`);
+
+// Задание 4
+
+a = 10;
+let b = 2;
+alert(`Сумма: ${a+b}, Разность: ${a-b}, Произведение: ${a*b}, Частное: ${a/b}`);
+
