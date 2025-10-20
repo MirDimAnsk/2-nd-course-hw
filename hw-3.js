6da63ddeaed8f3180213078e9d8416cdd9167cb1
MirDimAnsk <mda405@mail.ru>
2025-10-20 18:21:35 +0700

    JS task 3 completed

--- hw-3.js
+++ hw-3.js
@@ -16,3 +16,13 @@ if(c>0 && c<10) {
 } else{
     console.log('Неверно');
 }
+
+// Задание 3
+
+let d=101;
+let e=99;
+if(d>100 || e>100) {
+    console.log('Верно');
+} else{
+    console.log('Неверно');
+}
