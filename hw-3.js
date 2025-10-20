bee3bc722efb8cb50934463f8d14f63742ffd627
MirDimAnsk <mda405@mail.ru>
2025-10-20 17:53:53 +0700

    JS task 1 completed

---
+++ hw-3.js
@@ -0,0 +1,9 @@
+// Задание 1
+
+let password=String('password');
+let userPassword=String(prompt('Введите пароль'));
+if(password===userPassword){
+    alert('Пароль введен верно');
+} else{
+    alert('Пароль введен неправильно');
+}
