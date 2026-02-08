423f71ceb3779b1d30ae391527ede586b12fc32b
MirDimAnsk <mda405@mail.ru>
2026-02-08 16:30:25 +0000

    A running bar has been added, and the first game feature has been improved.

--- game.js
+++ game.js
@@ -3,6 +3,14 @@ function game1() {
     while (true) {
         let userNumber = prompt("Угадай число от 1 до 100");
         userNumber = Number(userNumber);
+        if (isNaN(userNumber)) {
+            alert("Пожалуйста, введи число!");
+            continue;
+        }
+        if (userNumber === null) {
+            alert('Игра отменена.');
+            break;
+        }
         if (userNumber === randomNumber) {
             alert("Поздравляю, ты угадал число!");
             break;
