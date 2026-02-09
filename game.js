e8e7a9549f4190b476d8d0eb39699ff63810486a
MirDimAnsk <mda405@mail.ru>
2026-02-09 12:26:29 +0700

    Correction function game1

--- game.js
+++ game.js
@@ -1,15 +1,16 @@
 function game1() {
-    const randomNumber = Math.floor(Math.random() * 100) + 1;
+    let randomNumber = Math.floor(Math.random() * 100) + 1;
     while (true) {
-        let userNumber = prompt("Угадай число от 1 до 100");
-        if (isNaN(userNumber)) {
-            alert("Пожалуйста, введи число!");
-            continue;
-        }
-        if (userNumber === null) {
+        let userInput = prompt("Угадай число от 1 до 100");
+        if (userInput === null) {
             alert('Игра отменена.');
             break;
         }
+        let userNumber = Number (userInput);
+        if (isNaN(userNumber)) {
+            alert("Пожалуйста, введи число!");
+            continue;
+        }        
         if (userNumber === randomNumber) {
             alert("Поздравляю, ты угадал число!");
             break;
