d45a1c1c4342f4b982c6d8df6119c7154cfc547d
MirDimAnsk <mda405@mail.ru>
2026-02-16 17:51:00 +0700

    Function for the second game is ready.

--- game.js
+++ game.js
@@ -3,7 +3,7 @@ function game1() {
     while (true) {
         let userInput = prompt("Угадай число от 1 до 100");
         if (userInput === null) {
-            alert('Игра отменена.');
+            alert("Игра отменена.");
             break;
         }
         let userNumber = Number (userInput);
@@ -19,5 +19,51 @@ function game1() {
         } else {
             alert("Твое число меньше загаданного, попробуй еще!");
         }
+    }
+}
+
+function game2() {
+    const operator = ['+', '-', '*', '/'];    
+    let operation = operator[Math.floor(Math.random() * operator.length)];
+    let a = Math.floor(Math.random() * 10) + 1;
+    let b = Math.floor(Math.random() * 10) + 1;
+    let userInput, userNumber, result;
+    switch (operation) {
+        case '+':                     
+            userInput = prompt (`Реши задачу: ${a} + ${b} введи ответ ниже`);
+            userNumber = Number (userInput);           
+            result = a + b;                    
+        break;
+        case '-':            
+            userInput = prompt (`Реши задачу: ${a} - ${b} введи ответ ниже`);           
+            userNumber = Number (userInput);
+            result = a - b;                       
+        break;
+        case '*':            
+            userInput = prompt (`Реши задачу: ${a} * ${b} введи ответ ниже`);
+            userNumber = Number (userInput);            
+            result = a * b;
+        break;
+        case '/':            
+            userInput = prompt (`Реши задачу ${a} / ${b} введи ответ ниже`);
+            userNumber = Number (userInput);            
+            result = a / b;            
+        break;
+    }
+    while (true) {
+        if (userInput === null) {
+            alert("Игра отменена.");
+            break;
+        }    
+        if (isNaN(userNumber)) {
+            alert("Игра отменена! Пожалуйста, вводи числа!");
+            break;            
+        } else if (userNumber === result) {
+                alert("Поздравляю, твой ответ верен!");
+                break;
+                } else {
+                    alert(`Ты ошибся, правильный ответ: ${result}!`);
+                    break;
+                    }
     }
 }
